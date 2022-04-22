b034545cb910a9c55a9e5278f882877378e21344
OlesyaSokolova <tsapa23@mail.ru>
2022-04-23 00:18:48 +0700

    ускорила заливку

--- frontend/web/js/layer/create_layer_publication.loader.js
+++ frontend/web/js/layer/create_layer_publication.loader.js
@@ -59,10 +59,27 @@ function prepareLayersToDraw() {
         const curColor = {
                 r: 0,
                 g: 0,
-                b: 0
+                b: 0,
+                a: 255
             };
-        var breakCycle = false;
-            var tmp = 10000;
+
+
+            //var outlineLayerData;
+            var colorLayerData;
+            var pixelStack = [];
+            var newColorR;
+            var newColorG;
+            var newColorB;
+            var newColorA;
+            var clickedColorR;
+            var clickedColorG;
+            var clickedColorB;
+            var clickedColorA;
+            var pixelsDrawn;
+
+            colorLayerData = context.getImageData(0, 0, canvas.width, canvas.height);
+        //var breakCycle = false;
+            //var tmp = 20000000;
             var counterPixels = 0;
 
         //1.5. init buttons
@@ -140,6 +157,7 @@ function prepareLayersToDraw() {
                 isErasing = false;
                 isDrawing = false;
                 isFilling = true;
+                colorLayerData = context.getImageData(0, 0, canvas.width, canvas.height);
                 fillArea(e.pageX - canvas.offsetLeft, e.pageY - canvas.offsetTop)
             }
         }
@@ -179,144 +197,139 @@ function prepareLayersToDraw() {
         }
 //http://www.williammalone.com/articles/html5-canvas-javascript-paint-bucket-tool/
         function fillArea(x, y) {
-            pixelsData = context.getImageData(0, 0, canvas.width, canvas.height);
-            var currentPixelIndex = (y * canvas.width + x) * 4;
-            const startR = pixelsData.data[currentPixelIndex];
-            const startG = pixelsData.data[currentPixelIndex + 1];
-            const startB = pixelsData.data[currentPixelIndex + 2];
-            const startA = pixelsData.data[currentPixelIndex + 3];//TODO:define alpha as current drawing setting
+            var pixelPos = (y*(canvas.width) + x)*4;
+            //console.log("outline: " + outlineLayerData.data[pixelPos] +","+ outlineLayerData.data[pixelPos+1] +","+ outlineLayerData.data[pixelPos+2] +","+ outlineLayerData.data[pixelPos+3]);
+            var r = colorLayerData.data[pixelPos + 0];
+            var g = colorLayerData.data[pixelPos + 1];
+            var b = colorLayerData.data[pixelPos + 2];
+            var a = colorLayerData.data[pixelPos + 3];
+            console.log("clicked color:   " + r +","+ g +","+ b +","+ a);
 
-            floodFill(x, y, startR, startG, startB, startA);
-        }
+            clickedColorR = r;
+            clickedColorG = g;
+            clickedColorB = b;
 
-        function floodFill(startX, startY, startR, startG, startB, startA) {
-            pixelsData = context.getImageData(0, 0, canvas.width, canvas.height);
-            var newPos,
-                x,
-                y,
-                pixelPos,
-                reachLeft,
-                reachRight,
-                drawingBoundLeft = backgroundX,
-                drawingBoundTop = backgroundY,
-                drawingBoundRight = backgroundX + canvas.width - 1,
-                drawingBoundBottom = backgroundY + canvas.height - 1,
-                pixelStack = [[startX, startY]];
+            //console.log("new color:   " + newColorR +","+ newColorG +","+ newColorB);
 
-            while (pixelStack.length && breakCycle === false) {
-
-                newPos = pixelStack.pop();
-                x = newPos[0];
-                y = newPos[1];
-
-                // Get current pixel position
-                pixelPos = (y * canvas.width + x) * 4;
-
-                // Go up as long as the color matches and are inside the canvas
-                while (y >= drawingBoundTop && breakCycle === false && matchStartColor(pixelPos, startR, startG, startB)) {
-                    y -= 1;
-                    pixelPos -= canvas.width * 4;
-                }
-
-                pixelPos += canvas.width * 4;
-                y += 1;
-                reachLeft = false;
-                reachRight = false;
-
-                // Go down as long as the color matches and in inside the canvas
-                while (y <= drawingBoundBottom && breakCycle === false && matchStartColor(pixelPos, startR, startG, startB)) {
-                    y += 1;
-
-                    colorPixel(pixelPos, curColor.r, curColor.g, curColor.b);
-
-                    if (x > drawingBoundLeft) {
-                        if (matchStartColor(pixelPos - 4, startR, startG, startB)) {
-                            if (!reachLeft) {
-                                // Add pixel to stack
-                                pixelStack.push([x - 1, y]);
-                                reachLeft = true;
-                            }
-                        } else if (reachLeft) {
-                            reachLeft = false;
-                        }
-                    }
-
-                    if (x < drawingBoundRight) {
-                        if (matchStartColor(pixelPos + 4, startR, startG, startB)) {
-                            if (!reachRight) {
-                                // Add pixel to stack
-                                pixelStack.push([x + 1, y]);
-                                reachRight = true;
-                            }
-                        } else if (reachRight) {
-                            reachRight = false;
-                        }
-                    }
-                    pixelPos += canvas.width * 4;
-                }
-            }
-        }
-
-        colorPixel = function (pixelPos, r, g, b, a) {
-            counterPixels++;
-            if(counterPixels === tmp)
+            /*if(clickedColorR == newColorR && clickedColorG == newColorG && clickedColorB == newColorB)
             {
-                breakCycle = true;
+                //console.log("Return because trying to fill with the same color");
+                return;
             }
-            console.log("fillPixel "  + pixelPos)
-           /* pixelsData = context.getImageData(0, 0, canvas.width, canvas.height);
-            pixelsData.data[pixelPos] = 255;
-            pixelsData.data[pixelPos + 1] = 255;
-            pixelsData.data[pixelPos + 2] = 0;
-            //pixelsData.data[pixelPos + 3] = 255;
-            context.putImageData(pixelsData, 0, 0 );*/
-            const image = context.getImageData(0, 0, canvas.width, canvas.height);
-            const {data} = image;
 
-            // red, green, blue, and alpha
-                data[pixelPos] = 0;//r
-                data[pixelPos + 1] = 0;//g
-                data[pixelPos + 2] = 0;//b
-            context.putImageData(image, 0, 0);
-        }
-      /*  matchOutlineColor = function (r, g, b, a) {
-
-            return (r + g + b < 100 && a === 255);
-        }*/
-
-        function matchStartColor(pixelPos, startR, startG, startB) {
-            var r = pixelsData.data[pixelPos],
-                g = pixelsData.data[pixelPos + 1],
-                b = pixelsData.data[pixelPos + 2],
-                a = pixelsData.data[pixelPos + 3];
-
-            // If current pixel of the outline image is black
-           /* if (matchOutlineColor(r, g, b, a)) {
-                return false;
+            if(outlineLayerData.data[pixelPos] + outlineLayerData.data[pixelPos+1] + outlineLayerData.data[pixelPos+2] == 0 && outlineLayerData.data[pixelPos+ 3] == 255)
+            {
+                //console.log("Return because clicked outline: " + outlineLayerData.data[pixelPos+4]);
+                return;
             }*/
 
-            r = pixelsData.data[pixelPos];
-            g = pixelsData.data[pixelPos + 1];
-            b = pixelsData.data[pixelPos + 2];
-            a = pixelsData.data[pixelPos + 3];
+            //console.log("PUSH: " + (startX - drawingAreaX - 2) + "," + (startY - drawingAreaY - 2));
+            pixelStack = [[x, y]];
 
-            // If the current pixel matches the clicked color
-            if (r === startR && g === startG && b === startB) {
-                return true;
+            floodFill();
+        }
+
+            function floodFill()
+            {
+                colorLayerData = context.getImageData(0, 0, canvas.width, canvas.height);
+                var newPos, x, y, pixelPos, reachLeft, reachRight;
+                var drawingBoundLeft = backgroundX;
+                var drawingBoundTop = backgroundY;
+                var drawingBoundRight = backgroundX + canvas.width - 1;
+                var drawingBoundBottom = backgroundY + canvas.height - 1;
+
+                while(pixelStack.length)
+                {
+                    newPos = pixelStack.pop();
+                    x = newPos[0];
+                    y = newPos[1];
+
+                    //console.log("POP: " + (x - drawingAreaX - 2) + "," + (y - drawingAreaY - 2));
+
+                    pixelPos = (y*(canvas.width) + x) * 4;
+                    // Go up as long as the color matches and are inside the canvas
+                    while(y-- >= drawingBoundTop && matchClickedColor(pixelPos))
+                    {
+                        //console.log("UP: " + (x - drawingAreaX - 2) + "," + (y - drawingAreaY - 2));
+                        pixelPos -= (canvas.width) * 4;
+                    }
+                    pixelPos += (canvas.width) * 4;
+                    ++y;
+                    reachLeft = false;
+                    reachRight = false;
+                    // Go down as long as the color matches and in inside the canvas
+                    while(y++ < drawingBoundBottom && matchClickedColor(pixelPos))
+                    {
+                        colorPixel(pixelPos);
+                        //console.log("COLOR: " + (x - drawingAreaX - 2) + "," + (y - drawingAreaY - 2));
+
+                        if(x > drawingBoundLeft)
+                        {
+                            if(matchClickedColor(pixelPos - 4)){
+                                if(!reachLeft){
+                                    pixelStack.push([x - 1, y]);
+                                    //console.log("PUSH: " + ((x-1) - drawingAreaX - 2) + "," + (y - drawingAreaY - 2));
+                                    reachLeft = true;
+                                }
+                            }else if(reachLeft){
+                                reachLeft = false;
+                            }
+                        }
+                        if(x < drawingBoundRight)
+                        {
+                            if(matchClickedColor(pixelPos + 4)){
+                                if(!reachRight){
+                                    pixelStack.push([x + 1, y]);
+                                    //console.log("PUSH: " + ((x+1) - drawingAreaX - 2) + "," + (y - drawingAreaY - 2));
+                                    reachRight = true;
+                                }
+                            }else if(reachRight){
+                                reachRight = false;
+                            }
+                        }
+                        pixelPos += canvas.width * 4;
+                    }
+                }
+                if(colorLayerData){
+                    context.putImageData(colorLayerData, 0, 0);
+                    colorLayerData = context.getImageData(0, 0, canvas.width, canvas.height);
+                }
             }
-           /* if(a === 0) {
-                return true;
-            }*/
 
-            // If current pixel matches the new color
-            if (r === curColor.r && g === curColor.g && b === curColor.b) {
+            function matchClickedColor(pixelPos)
+            {
+                var r = colorLayerData.data[pixelPos];
+                var g = colorLayerData.data[pixelPos+1];
+                var b = colorLayerData.data[pixelPos+2];
+                var a = colorLayerData.data[pixelPos+3];
+
+                // If current pixel is black then it is an outline
+                if(r + g + b === 0 && a === 255){ return false; }
+
+                r = colorLayerData.data[pixelPos];
+                g = colorLayerData.data[pixelPos+1];
+                b = colorLayerData.data[pixelPos+2];
+
+                // If the current pixel matches the clicked color
+                if(r === clickedColorR && g === clickedColorG && b === clickedColorB) return true;
+
+                // If current pixel matches the new color
+                if(r === 0 && g === 0 && b === 0) return false;
+
                 return false;
             }
 
-            return true;
-        }
+            function colorPixel(pixelPos)
+            {
+                console.log("color pixel")
+                colorLayerData.data[pixelPos] = 0;
+                colorLayerData.data[pixelPos+1] = 0;
+                colorLayerData.data[pixelPos+2] = 0;
+                colorLayerData.data[pixelPos+3] = 255;
+            }
 
-        toolbarClassContainer = 'toolbar'
+
+            toolbarClassContainer = 'toolbar'
 
         $('.' + toolbarClassContainer)
             .on('input change', '.color-value', function () {
