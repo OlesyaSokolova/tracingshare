9785c6aad8684d06d61b0215cb44be244d46ae1e
OlesyaSokolova <tsapa23@mail.ru>
2022-06-17 00:24:18 +0700

    исправила регистрацию

--- frontend/models/SignupForm.php
+++ frontend/models/SignupForm.php
@@ -84,7 +84,8 @@ class SignupForm extends Model
                 ['html' => 'emailVerify-html', 'text' => 'emailVerify-text'],
                 ['user' => $user]
             )
-            ->setFrom([Yii::$app->params['supportEmail'] => Yii::$app->name . ' robot'])
+            //->setFrom([Yii::$app->params['supportEmail'] => Yii::$app->name . ' robot'])
+            ->setFrom('tracingshare@yandex.ru')
             ->setTo($this->email)
             ->setSubject('Подтверждение регистрации ' . Yii::$app->name)
             ->send();
