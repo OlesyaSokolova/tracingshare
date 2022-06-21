e6ba13e5f8274a19861b9b80e94891c44807e5f1
OlesyaSokolova <tsapa23@mail.ru>
2022-06-21 16:42:51 +0700

    добавила CKEditor

--- frontend/views/publication/uploadOriginalImage.php
+++ frontend/views/publication/uploadOriginalImage.php
@@ -5,17 +5,28 @@
 
 use yii\bootstrap4\Html;
 use yii\bootstrap4\ActiveForm;
+use mihaildev\ckeditor\CKEditor;
 
 $this->title = 'Создание публикации';
 ?>
 <div class="publication-upload-original-image">
     <h3 style="text-align: center;"><?= Html::encode($this->title) ?></h3>
     <div class="row justify-content-center">
-        <div class="col-lg-5">
+        <div class="col-lg-8">
 
             <?php $form = ActiveForm::begin(['id' => 'form-upload-original-image', 'options' => ['enctype' => 'multipart/form-data']]) ?>
             <?= $form->field($model, 'name')->label("Название:") ?>
-            <?= $form->field($model, 'description')->textarea(['rows' => '6'])->label("Описание:") ?>
+            <?= $form->field($model, 'description')->textarea(['rows' => '8'])->label("Описание:")->widget(CKEditor::className(),
+                [
+                    'editorOptions' => [
+                        'preset' => 'standard',
+                        'inline' => false,
+                    ],
+                    'options' => [
+                        'allowedContent' => true,
+                    ],
+
+                ]) ?>
             <?= $form->field($model, 'imageFile')->fileInput()->label("Изображение:") ?>
 
             <div class="form-group" style="text-align: center;">
